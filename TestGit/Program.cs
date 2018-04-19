f519415f852cd07086c79a87890eb417fa42ca27
Sodipto saha <badhansaha444@gmail.com>
2018-04-19 20:44:42 +0600

    program.cs change

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -10,6 +10,11 @@ namespace TestGit
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hello Github");
+            Console.ReadKey();    
+
+
         }
     }
 }
